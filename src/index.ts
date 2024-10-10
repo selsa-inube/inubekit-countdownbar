7eccc24bd85713819107e50d2a7406c083fe1129
Cesar Marin Alfonso <45011420+cmarin001@users.noreply.github.com>
2024-10-10 11:58:40 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { CountdownBar } from "./CountdownBar";
 export type { ICountdownBar } from "./CountdownBar";
 export type { ICountdownBarAppearance } from "./CountdownBar/props";
+export { tokens as CountdownBarTokens } from "./CountdownBar/Tokens/tokens";
